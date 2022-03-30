a1fc459bb70c446816c707e85b9b61cedab158dc
Anil <meghanilva@live.in>
2022-03-30 23:33:39 +0530

    switch browsers do

--- src/test/java/org/selenium/factory/Factory.java
+++ src/test/java/org/selenium/factory/Factory.java
@@ -2,6 +2,9 @@ package org.selenium.factory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Factory {
 //public WebDriver initDriver() {
@@ -9,7 +12,20 @@ public class Factory {
 //}
 
 public WebDriver initDriver() {
-	// TODO Auto-generated method stub
-	return new ChromeDriver();
+	String browser = System.getProperty("browser");
+	WebDriver driver;
+	switch(browser) {
+	case "chrome" :
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		break;
+	case "firefox":
+		WebDriverManager.firefoxdriver().setup();
+		driver=new FirefoxDriver();
+		break;
+		default:
+			throw new IllegalStateException("not a browser"	);
+	}
+	return driver;
 }
 }
