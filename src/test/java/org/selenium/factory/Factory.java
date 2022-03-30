5bbcf437e472c425c9631e98108b66fbeaa48cc6
Anil <meghanilva@live.in>
2022-03-30 17:07:35 +0530

    run on diff browsers

--- src/test/java/org/selenium/factory/Factory.java
+++ src/test/java/org/selenium/factory/Factory.java
@@ -2,14 +2,36 @@ package org.selenium.factory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Factory {
 //public WebDriver initDriver() {
 //	return new ChromeDriver();
 //}
-
+WebDriver driver;
 public WebDriver initDriver() {
 	// TODO Auto-generated method stub
-	return new ChromeDriver();
+	String browser = System.getProperty("browser");
+	browser="FIREFOX";
+	switch(browser) {
+	case  "CHROME" :
+		WebDriverManager.chromedriver();
+		driver= new ChromeDriver();
+		break;
+	case "FIREFOX" :
+		WebDriverManager.firefoxdriver();
+		driver= new FirefoxDriver();
+		break;
+	default:
+		WebDriverManager.operadriver();
+		driver= new OperaDriver();
+		break;
+	}
+	
+	return driver;
+
 }
 }
